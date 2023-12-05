a1043e5c629770a62189ce3b1e35e074b548c278
kloo <tehpiratezor@gmail.com>
2023-12-05 00:43:27 -1000

    Day 5 late start
    5a done after 3 hour late start, 5b in prog with general idea but issues with PS handling 2d array. Should convert to String array or ordered dictionary

--- 5/a_2023.ps1
+++ 5/a_2023.ps1
@@ -1,23 +1,73 @@
 <#
-
+Get seeds and translate to locations
 #>
 
 $strarray = Get-Content ina.txt
-$answer = 0
+$answer = [int64]::MaxValue
 
 $inputLen = $strarray.Length
-$inputArray = New-Object int[] $inputLen
+$inputArray = New-Object String[] $inputLen
 
-Function Test-Func {
+Function Get-TranslationBlock {
     Param (
-        [System.Collections.Specialized.OrderedDictionary] $playerOneDeck,
-        [System.Collections.Specialized.OrderedDictionary] $playerTwoDeck
+        [int64] $startLine
     )
+
+    $currLine = $startLine
+
+    while (($strarray[$currLine] -ne "") -and ($currLine -lt $strarray.Length)) {
+        $currLine++
+    }
     
+    return $currLine - 1
 }
 
-foreach ($line in $strarray) {
-    
+$trash, $seeds = $strarray[0].Split(" ")
+$seeds = [int64[]] $seeds
+
+$translationMapCount = 7
+$translations = 0
+
+$line = 3;
+$translationBlocks = [int64[]] @()
+
+while ($translations -lt $translationMapCount ) {
+
+    $endLine = Get-TranslationBlock $line
+
+    $translationBlocks += $line
+    $translationBlocks += $endLine
+
+    $line = $endLine + 3 #skip the blank line and header
+
+    $translations++
 }
+
+ForEach ($seed in $seeds) {
+
+    $currPoint = $seed
+    
+    for ($i = 0;$i -lt $translationBlocks.Length;$i+=2) { # iterate through each translation set
+
+        for ($j = $translationBlocks[$i];$j -le $translationBlocks[$i+1];$j++) { # iterate through each line of a translation set
+            $currLine = $strarray[$j].Split(" ")
+
+            $dest = [int64] $currLine[0]
+            $source = [int64] $currLine[1]
+            $sourceEnd = $source + ([int64] $currLine[2]) #goes 1 over actual range so use lt on comparison below
+
+            if ( ($currPoint -ge $source) -and ($currPoint -lt $sourceEnd) ) {
+                $currPoint = $dest + ($currPoint - $source)
+                break;
+            }
+        }
+
+    }
+
+    if ($currPoint -lt $answer) {
+        $answer = $currPoint
+    }
+}
+
 
 Write-Host $answer
--- 5/b_2023.ps1
+++ 5/b_2023.ps1
@@ -1,15 +1,116 @@
 <#
-
+Get seed ranges and translate to locations
 #>
 
-$strarray = Get-Content inb.txt
-$answer = 0
+$strarray = Get-Content testin.txt
+$answer = [int64]::MaxValue
 
 $inputLen = $strarray.Length
-$inputArray = New-Object int[] $inputLen
+$inputArray = New-Object String[] $inputLen
 
-foreach ($line in $strarray) {
+Function Get-TranslationBlock {
+    Param (
+        [int64] $startLine
+    )
+
+    $currLine = $startLine
+
+    while (($strarray[$currLine] -ne "") -and ($currLine -lt $strarray.Length)) {
+        $currLine++
+    }
     
+    return $currLine - 1
+}
+
+Function Translate-Block {
+    Param (
+        [int64[]] $inputArray #Array of Translation Block Number, Start Number, Range
+    )
+
+    $blockStart = $inputArray[0]*2
+    $blockEnd = $blockStart + 1
+
+    $blockStart = $translationBlocks[$blockStart]
+    $blockEnd = $translationBlocks[$blockEnd]
+
+    $startNum = $inputArray[1]
+    $endNum = $startNum + $inputArray[2] - 1 # Subtract 1 to match exact range
+
+    for ($j = $blockStart;$j -le $blockEnd;$j++) { # iterate through each line of a translation set
+            $currLine = $strarray[$j].Split(" ")
+
+            $dest = [int64] $currLine[0]
+            $source = [int64] $currLine[1]
+            $sourceEnd = $source + ([int64] $currLine[2]) - 1 # Subtract 1 to match exact range
+
+            $coversLow = $source -le $startNum
+            $coversHigh = $sourceEnd -ge $endNum
+            
+            $subsetLow = $coversLow -and ($sourceEnd -ge $startNum)
+            $subsetHigh = $coversHigh -and ($source -le $endNum )
+
+            $subsetCenter = ($source -le $endNum) -and ($sourceEnd -ge $startNum)
+
+            if ( $coversLow -and $coversHigh ) { # If translation range encompasses full input range
+                $queue
+            } elseif ( $subsetLow ) { # If translation range covers lower end of input range
+                
+            } elseif ( $subsetHigh ) { # If translation range cover upper end of input range
+                
+            } elseif ( $subsetCenter ) { # If translation range covers some center subset of input range
+                
+            }
+    }
+}
+
+$trash, $seeds = $strarray[0].Split(" ")
+$seeds = [int64[]] $seeds
+
+$queue = @()
+$endQueue = @()
+
+for ($i = 0; $i -lt $seeds.Length;$i+=2) {
+    $queue += @(0,$seeds[$i],$seeds[$i+1])
+}
+
+$translationMapCount = 7
+$translations = 0
+
+$line = 3;
+$translationBlocks = [int64[]] @()
+
+while ($translations -lt $translationMapCount ) {
+
+    $endLine = Get-TranslationBlock $line
+
+    $translationBlocks += $line
+    $translationBlocks += $endLine
+
+    $line = $endLine + 3 #skip the blank line and header
+
+    $translations++
+}
+
+while($queue.Count -gt 0) {
+    Translate-Block $queue[0]
+    
+    if ($queue[0][2] -eq $null) { # Handle case of one array in 2d array crunching to 1d array
+        $currArray = $queue[0]
+        $queue = @()
+
+        Translate-Block $currArray
+    } else {
+        Translate-Block $queue[0]
+        $trash, $queue = $queue
+    }
+}
+
+ForEach ($locationRange in $endQueue) {
+    $min = $locationRange[1]
+
+    if ($min -lt $answer) {
+        $answer = $min
+    }
 }
 
 Write-Host $answer
